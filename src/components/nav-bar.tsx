8fe138f05985abc42c313ccb4541c4ed0c99c9ac
Hugo Cruz <hugocruzlfc@gmail.com>
2025-05-19 17:27:22 +0200

    feat: add navbar component

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import logo from "@/assets/logo.png";
+import { ROUTES } from "@/lib/constant";
+import { UserButton } from "@clerk/nextjs";
+import { CreditCard } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="shadow-sm">
+      <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
+        <Link href={ROUTES.RESUMES} className="flex items-center gap-2">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+          <span className="text-xl font-bold tracking-tight">
+            AI Resume Builder
+          </span>
+        </Link>
+        <UserButton
+          appearance={{
+            elements: {
+              avatarBox: {
+                width: 35,
+                height: 35,
+              },
+            },
+          }}
+        >
+          <UserButton.MenuItems>
+            <UserButton.Link
+              label="Billing"
+              labelIcon={<CreditCard className="size-4" />}
+              href={ROUTES.BILLING}
+            />
+          </UserButton.MenuItems>
+        </UserButton>
+      </div>
+    </header>
+  );
+}

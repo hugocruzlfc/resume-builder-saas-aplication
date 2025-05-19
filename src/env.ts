2f36618cda4c4def2560ab24d2ce0392ef5215b5
Hugo Cruz <hugocruzlfc@gmail.com>
2025-05-19 17:12:32 +0200

    feat: add clerk provider and auth pages

--- src/env.ts
+++ src/env.ts
@@ -10,6 +10,8 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_URL: z.string().min(1),
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1),
   },
 
   runtimeEnv: {
@@ -19,5 +21,7 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
   },
 });

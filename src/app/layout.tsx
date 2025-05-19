560ab0f9e85ead947c1df8885ac6cffe3c08a758
Hugo Cruz <hugocruzlfc@gmail.com>
2025-05-19 14:22:45 +0200

    style: add color design and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nextjs Perfect Base Template",
-  description: "A perfect base template for Next.js applications",
+  title: {
+    template: "%s - AI Resume Builder",
+    absolute: "AI Resume Builder",
+  },
+  description:
+    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
 };
 
 export default function RootLayout({
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         {children}
         <Toaster />
       </body>
--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    BLOB_READ_WRITE_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_URL: z.string().min(1),
@@ -12,5 +13,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
   },
 });

7c4edb7430a01ba4e3abf2ff7980cc9a52bca139
Hugo Cruz <hugocruzlfc@gmail.com>
2025-06-27 14:42:21 +0200

    feat: add env variables safe

--- src/env.ts
+++ src/env.ts
@@ -7,12 +7,23 @@ export const env = createEnv({
     BLOB_READ_WRITE_TOKEN: z.string().min(1),
     CLERK_SECRET_KEY: z.string().min(1),
     OPENAI_API_KEY: z.string().min(1),
+    POSTGRES_PRISMA_URL: z.string().min(1),
+    POSTGRES_URL_NO_SSL: z.string().min(1),
+    POSTGRES_URL_NON_POOLING: z.string().min(1),
+    POSTGRES_USER: z.string().min(1),
+    POSTGRES_HOST: z.string().min(1),
+    POSTGRES_PASSWORD: z.string().min(1),
+    POSTGRES_DATABASE: z.string().min(1),
+    STRIPE_SECRET_KEY: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_BASE_URL: z.string().min(1),
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1),
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_MONTHLY: z.string().min(1),
+    NEXT_PUBLIC_BASE_URL: z.string().min(1).url(),
   },
 
   runtimeEnv: {
@@ -25,5 +36,19 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY:
+      process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_MONTHLY,
+    NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_MONTHLY:
+      process.env.NEXT_PUBLIC_STRIPE_PRICE_ID_PRO_PLUS_MONTHLY,
   },
 });
